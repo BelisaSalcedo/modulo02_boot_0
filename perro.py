87e98c53c25b4e159ed28a6986a54e9bc3bec25e
BelisaSalcedo <97406760+BelisaSalcedo@users.noreply.github.com>
2022-01-11 21:03:10 +0100

    perro segunda version

--- perro.py
+++ perro.py
@@ -3,3 +3,11 @@ class Perro ():
         self.nombre=n
         self.edad=e
         self.peso=p
+    def ladrar (loquesea):
+        if loquesea.peso >= 8:
+            print ('¡GUA GUAUU!')
+        else:
+            print('Gua guau')
+    def __str__(self):
+        return 'Perro {}, e:{}, p{}'.format(self.nombre,self.edad,self.peso)
+    
