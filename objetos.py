5b3578108c75baa4786597574b9462da1b96dd10
BelisaSalcedo <97406760+BelisaSalcedo@users.noreply.github.com>
2022-01-11 20:26:35 +0100

    mas tortuga

--- objetos.py
+++ objetos.py
@@ -2,11 +2,16 @@ import turtle
 
 tortuguita= turtle.Turtle()
 otraTortuguita=turtle.Turtle()
+lenta=turtle.Turtle()
+
+lenta.speed(1)
 
 tortuguita.shape('turtle')
 tortuguita.color('blue')
 tortuguita.fd(50)
+tortuguita.speed(5)
 
 otraTortuguita.color('green')
 otraTortuguita.left(90)
 otraTortuguita.fd(90)
+otraTortuguita.speed(5)
